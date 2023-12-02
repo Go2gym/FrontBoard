a8dee7e5693ef57b98de41eb1dd530e1af7b7d01
BubblingSODA <go2gym365@gmail.com>
2023-12-02 11:47:28 +0000

    Make modal and add to map

--- crush-course/src/components/PostList.jsx
+++ crush-course/src/components/PostList.jsx
@@ -1,12 +1,37 @@
 import Post from "./Post";
+import NewPost from "./NewPost";
+import Modal from "./Modal";
 import classes from "./PostList.module.css";
+import { useState } from "react";
 
-function PostList() {
+function PostList({ isPosting, onStopPosting }) {
+  const [posts, setPosts] = useState([]);
+
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!" />
-    </ul>
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+    </>
   );
 }
 
